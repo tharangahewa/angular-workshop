54629d6b1a8250e5c66a7995172eb83d97771a94
Tharanga Hewavithana <tharanga.hewavithana@cinglevue.com>
2020-03-30 12:21:22 +0530

    Added components

--- the-study-mate/src/app/app.module.ts
+++ the-study-mate/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FiltersComponent } from './filters/filters.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FooterComponent } from './footer/footer.component';
+import { CourseComponent } from './courses/course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CarouselComponent,
+    FiltersComponent,
+    CoursesComponent,
+    FooterComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
