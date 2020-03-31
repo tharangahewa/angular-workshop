631f60c9c2527ae828a7c36ca909ad2e7764ce6f
Tharanga Hewavithana <tharanga.hewavithana@cinglevue.com>
2020-03-31 09:48:00 +0530

    Bound data to the view

--- the-study-mate/src/app/courses/course/course.component.ts
+++ the-study-mate/src/app/courses/course/course.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
+  //model
+  course = {
+    title: 'Angular Workshop',
+    imgUrl: 'assets/angular-3-logo.png',
+    price: 50,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+    starRating: 4
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval( ()=> {
+      this.course.price = Math.round( Math.random() * 100);
+    }, 2000);
+  }
+
+  onClick(event: Event){
+    this.course.price += 10;
   }
 
 }
